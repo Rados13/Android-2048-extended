21ac664cb196f263d062bf744505a1b8ae3ad6ff
unknown <radoslawszuma@gmail.com>
2020-02-22 13:36:23 +0100

    Blocks have colors based on value

--- app/src/main/java/com/example/a2048/MapGamePainter.java
+++ app/src/main/java/com/example/a2048/MapGamePainter.java
@@ -27,7 +27,7 @@ public class MapGamePainter extends Drawable {
         greyBeginPaint = new Paint();
         brownPaint = new Paint();
         darkPaint = new Paint();
-        greyBeginPaint.setARGB(255, 169, 169, 169);
+        greyBeginPaint.setARGB(255, 170, 170, 170);
         brownPaint.setARGB(255, 96, 96, 96);
         darkPaint.setARGB(255, 0, 0, 0);
     }
@@ -53,6 +53,8 @@ public class MapGamePainter extends Drawable {
 
                 } else if (mapStructure[i][j] >= 0) {
 
+
+                    changeGreyColor(mapStructure[i][j]);
 
                     canvas.drawRect((float) ((j + gap) * oneRect),
                             (float) ((i + gap) * oneRect),
@@ -101,8 +103,47 @@ public class MapGamePainter extends Drawable {
         return PixelFormat.OPAQUE;
     }
 
-    Integer[][] getMap() {
-        return mapStructure;
+    private void changeGreyColor(int val) {
+        int red = 170;
+        int green = 170;
+        int blue = 170;
+        int changeR = 6;
+        int changeG = -30;
+        int changeB = -30;
+        while (val > 1) {
+            red += changeR;
+            green += changeG;
+            blue += changeB;
+            if (red == 200 && blue == 20 && green == 20) {
+                changeG = 42;
+                changeB = -6;
+                blue += 10;
+            } else if (red == 230 && blue == 0 && green == 230) {
+                blue += 10;
+                changeR = -40;
+                changeB = 4;
+                changeG = 0;
+            } else if (red == 30 && blue == 30 && green == 230) {
+                green -= 30;
+                green += 20;
+                changeR = -6;
+                changeB = 40;
+                changeG = -40;
+            } else if (red == 0 && blue == 250 && green == 0) {
+                changeR = 10;
+                changeB = -40;
+                changeG = 10;
+            } else if (red == 50 && blue == 50 && green == 50) {
+                red = 170;
+                green = 170;
+                blue = 170;
+                changeR = 6;
+                changeG = -30;
+                changeB = -30;
+            }
+            val--;
+        }
+        greyBeginPaint.setARGB(255, red, green, blue);
     }
 
 
